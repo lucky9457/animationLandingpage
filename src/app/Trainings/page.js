651dda11c1a46e364a14ad8ff5a0bdcc603e8f62
chandusiriyala <chandusiriyala116@gmail.com>
2024-12-05 04:01:46 +0530

    changes 5/12/2024 4:01AM done cards in servicespage

--- src/app/Trainings/page.js
+++ src/app/Trainings/page.js
@@ -8,21 +8,21 @@ import { motion } from 'framer-motion';
 
 const testimonials = [
   {
-    name: "Melissa Hurtado",
-    text: "Excited to highlight the exceptional talents of Rukshana, an incredible professional and dear friend! Rukshana of Sabeena Digital Media Services effortlessly wears many hats, showcasing exceptional skills in social media management, storytelling, and crafting engaging content for newsletters and marketing emails. Working alongside Rukshana is an absolute delight. The positive energy she brings to every collaboration is infectious, and her dedication to excellence is truly commendable! ",
-    position: "Property Manager, Self employed ",
+    name: "Mohamed Ridwan A",
+    text: "The 15-day inplant training on UI/UX design with Figma by Sabeena Digital Media Services was incredible. By the end of just 13 days, I had designed an entire UI/UX site with confidence! Tutor Bala’s teaching was clear, practical, and engaging, making complex features like animations easy to master.With a hands-on approach and a supportive team, this training has truly elevated my design skills. Highly recommended for anyone looking to excel in UI/UX design",
+    position: "UI/UX In-Plant Training Attendee",
     image: "./images/MelissaHurta.jpeg"
   },
   {
-    name: "Mariel McCann",
-    text: "Rukshana of Sabeena Digital Media Services is a creative and talented social media content creator and manager! Rukshana has helped our 501c3 Shooting Stars Foundation by creating gorgeous collateral with high impact. She is patient and values all requests and feedback. She is an integral part of our social media and digital design strategy. Thank you, Rukshana for your passion, dedication",
-    position: "Shooting Stars (Former) Senior Program Manager",
+    name: "Kaviya",
+    text: "The UI/UX In-Plant training was a fantastic experience. Initially, it felt challenging to perfect my designs, but the tutor’s expert guidance and practical tips made all the difference.The training broke down complex concepts into simple steps, and by the end, I was able to create a complete UI/UX project confidently. It was an enriching journey that sharpened my skills and transformed the way I approach design. Highly recommended for aspiring designers!",
+    position: "UI/UX In-Plant Training Attendee",
     image: "./images/Mariel.jpeg"
   },
   {
-    name: "Vidhya",
-    text: "We had the pleasure of working with Rukshana as our digital marketing partner at Right Dots, and her contributions were invaluable. Rukshana was instrumental in driving the marketing efforts for our Learning Series, showcasing her expertise in reaching and engaging our target audience. Her support during the sessions was exceptional; she not only helped us craft compelling content but also ensured that everything ran smoothly. Rukshana's creativity, professionalism, and dedication truly made a difference in the success of our initiative. We look forward to collaborating with her again in the future!",
-    position: "Founder, Right Dots",
+    name: "Rifkhan",
+    text: "The front-end development training at Sabeena Digital Media Services was absolutely worth every penny. The course gave me the skills to truly excel in front-end development, from mastering core concepts to working with advanced techniques.The practical, hands-on approach and expert guidance made complex topics easy to grasp. I now feel fully equipped to take on real-world projects with confidence. I highly recommend this program to anyone looking to sharpen their front-end development skills!!",
+    position: "Full-Stack Training Attendee",
     image: "./images/vidhya.jpg"
   },
   {
@@ -129,7 +129,7 @@ export default function Trainings() {
 
          </div>
          <div className="testimonials-container" id = "testimonials">
-      <h2 className="testimonials-heading">student's<br/>testimony</h2>
+      <h2 className="testimonials-heading">participant's<br/>testimony</h2>
       <motion.div className="testimonials-wrapper" drag="x" dragConstraints={{ left: -1500, right: 0 }}>
         {testimonials.map((testimonial, index) => (
           <motion.div
