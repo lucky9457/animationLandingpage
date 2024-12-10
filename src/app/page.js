8e12252fb7170f233e9d368de293c7436cbadf31
chandusiriyala <chandusiriyala116@gmail.com>
2024-12-10 23:13:52 +0530

    changes done

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Page({darkMode}) {
     return (
         
         <div>
-           
+            <Cursor darkMode={darkMode}/>
                 <Home />
                 <Services />
                 <Testimonials />
